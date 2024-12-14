448074dc596c7c5496953850bbc0b3f46bd41f85
Nicholas Pettas <nick@faircrypto.org>
2024-12-13 17:16:37 -0800

    Update the CI pipeline to allow for concurrent agents on one machine
    Start using the Buildkit docker plugin, to handle containers better.
    No more killing stale containers, which prevents jobs from running concurrently.

--- ci/buildkite-pipeline.sh
+++ ci/buildkite-pipeline.sh
@@ -6,16 +6,22 @@
 set -e
 cd "$(dirname "$0")"/..
 
+source ci/env.sh
+
 output_file=${1:-/dev/stderr}
 
 if [[ -n $CI_PULL_REQUEST ]]; then
-  # filter pr number from ci branch.
-  [[ $CI_BRANCH =~ pull/([0-9]+)/head ]]
-  pr_number=${BASH_REMATCH[1]}
-  echo "get affected files from PR: $pr_number"
+	if [[ -n $BUILDKITE_PULL_REQUEST ]]; then
+		pr_number=$BUILDKITE_PULL_REQUEST
+	else
+		# filter pr number from ci branch.
+		[[ $CI_BRANCH =~ pull/([0-9]+)/head ]]
+		pr_number=${BASH_REMATCH[1]}
+	fi
+	echo "get affected files from PR: $pr_number"
 
   # get affected files
-  readarray -t affected_files < <(gh pr diff --name-only "$pr_number")
+  readarray -t affected_files < <(GH_TOKEN="$(buildkite-agent secret get GH_TOKEN)" gh pr diff --name-only "$pr_number")
   if [[ ${#affected_files[*]} -eq 0 ]]; then
     echo "Unable to determine the files affected by this PR"
     exit 1
@@ -118,6 +124,30 @@ command_step() {
 EOF
 }
 
+docker_command_step() {
+  cat >> "$output_file" <<EOF
+  - name: "$1"
+    command: "$2"
+    plugins:
+      - docker#v5.12.0:
+          image: "$3"
+          workdir: /solana
+          propagate-environment: true
+          propagate-uid-gid: true
+          environment:
+            - "RUSTC_WRAPPER=/usr/local/cargo/bin/sccache"
+            - BUILDKITE_AGENT_ACCESS_TOKEN
+            - AWS_SECRET_ACCESS_KEY
+            - AWS_ACCESS_KEY_ID
+            - SCCACHE_BUCKET
+            - SCCACHE_REGION
+            - SCCACHE_S3_KEY_PREFIX
+    timeout_in_minutes: $4
+    artifact_paths: "log-*.txt"
+    agents:
+      queue: "${5:-solana}"
+EOF
+}
 
 trigger_secondary_step() {
   cat  >> "$output_file" <<"EOF"
@@ -140,10 +170,11 @@ wait_step() {
 }
 
 all_test_steps() {
-  command_step checks1 "ci/docker-run-default-image.sh ci/test-checks.sh" 20 check
-  command_step checks2 "ci/docker-run-default-image.sh ci/test-dev-context-only-utils.sh check-bins" 15 check
-  command_step checks3 "ci/docker-run-default-image.sh ci/test-dev-context-only-utils.sh check-all-targets" 30 check
-  command_step miri "ci/docker-run-default-image.sh ci/test-miri.sh" 5 check
+  . ci/rust-version.sh
+  docker_command_step checks1 "ci/test-checks.sh" "$ci_docker_image" 20 check
+  docker_command_step checks2 "ci/test-dev-context-only-utils.sh check-bins" "$ci_docker_image" 20 check
+  docker_command_step checks3 "ci/test-dev-context-only-utils.sh check-all-targets" "$ci_docker_image" 30 check
+  docker_command_step miri "ci/test-miri.sh" "$ci_docker_image" 5 check
   wait_step
 
   # Full test suite
@@ -157,7 +188,7 @@ all_test_steps() {
              ^ci/rust-version.sh \
              ^ci/test-docs.sh \
       ; then
-    command_step doctest "ci/docker-run-default-image.sh ci/test-docs.sh" 15
+    docker_command_step doctest "ci/test-docs.sh" "$ci_docker_image" 15
   else
     annotate --style info --context test-docs \
       "Docs skipped as no .rs files were modified"
@@ -183,7 +214,20 @@ all_test_steps() {
              cargo-test-sbf$ \
       ; then
     cat >> "$output_file" <<"EOF"
-  - command: "ci/docker-run-default-image.sh ci/test-stable-sbf.sh"
+  - command: "ci/test-stable-sbf.sh"
+    plugins:
+      - docker#v5.12.0:
+          image: "$ci_docker_image"
+          workdir: /solana
+          propagate-environment: true
+          propagate-uid-gid: true
+          environment:
+            - "RUSTC_WRAPPER=/usr/local/cargo/bin/sccache"
+            - AWS_SECRET_ACCESS_KEY
+            - AWS_ACCESS_KEY_ID
+            - SCCACHE_BUCKET
+            - SCCACHE_REGION
+            - SCCACHE_S3_KEY_PREFIX
     name: "stable-sbf"
     timeout_in_minutes: 35
     artifact_paths: "sbf-dumps.tar.bz2"
@@ -227,7 +271,7 @@ EOF
              ^ci/test-stable.sh \
              ^sdk/ \
       ; then
-    command_step wasm "ci/docker-run-default-image.sh ci/test-wasm.sh" 20
+    docker_command_step wasm "ci/test-wasm.sh" "$ci_docker_image" 20
   else
     annotate --style info \
       "wasm skipped as no relevant files were modified"
@@ -259,7 +303,7 @@ EOF
              ^ci/test-coverage.sh \
              ^scripts/coverage.sh \
       ; then
-    command_step coverage "ci/docker-run-default-image.sh ci/test-coverage.sh" 80
+    docker_command_step coverage "ci/test-coverage.sh" "$ci_docker_image" 80
   else
     annotate --style info --context test-coverage \
       "Coverage skipped as no .rs files were modified"
@@ -297,7 +341,7 @@ pull_or_push_steps() {
 
     if [ -z "$diff_other_than_version_bump" ]; then
       echo "Diff only contains version bump."
-      command_step checks "ci/docker-run-default-image.sh ci/test-checks.sh" 20
+      docker_command_step checks "ci/test-checks.sh" "$ci_docker_image" 20
       exit 0
     fi
   fi
